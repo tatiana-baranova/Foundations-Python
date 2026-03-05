a6790d9da3c5bafc952a8000bd84679e71f226d3
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-05 22:07:16 +0200

    independent work on data list

--- chapter_02/task1.py
+++ chapter_02/task1.py
@@ -1,20 +1,32 @@
 # Списки данних
 
-data = [5, 2.3, True, "Hello", [4, 7]]
-# data[2] = False
-numbers = [9, 5, 2]
-data.append("info")
-data.extend(numbers)
-data.insert(1, [4, 5, 6])
+# data = [5, 2.3, True, "Hello", [4, 7]]
+# # data[2] = False
+# numbers = [9, 5, 2]
+# data.append("info")
+# data.extend(numbers)
+# data.insert(1, [4, 5, 6])
 
-data.remove('info')
-data.pop()
+# data.remove('info')
+# data.pop()
 
-numbers.sort()
-numbers.reverse()
-print(numbers)
+# numbers.sort()
+# numbers.reverse()
+# print(numbers)
 
-# data.clear()
-print(data.count(5))
-print(len(data))
+# # data.clear()
+# print(data.count(5))
+# print(len(data))
 
+# Самостійна робота
+
+fruits = ["apple", "banana", "orange"]
+
+fruits.append("lemon")
+print("list length: ", len(fruits))
+fruits.insert(len(fruits) // 2, "pear")
+print(fruits)
+fruits.remove("banana")
+print("Index orange: ", fruits.index("orange"))
+fruits.sort()
+print(fruits)
