af8c68026859e929c42af0a9e4fa33cc1fa5c1d7
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-05 14:18:19 +0200

    information output

--- chapter_01/task1.py
+++ chapter_01/task1.py
@@ -1,1 +1,6 @@
 print('Hello Word')
+name = "Таня"
+age = 28
+print("Ім'я:", name, "Вік:", age)
+print("Python", "це", "перкрасно", sep='|', end='!\n')
+print('Привіт!', 'Мене звуть Тетяна', 'і мені 28 років.', sep=' ', end='\n')
---
+++ chapter_01/task2.py
@@ -0,0 +1,12 @@
+info = 50
+print("Result: ", info, "!", sep='')
+# del info
+info = 7
+print(info)
+
+# Типи данних
+a1: int = 500 #int Integer
+a2: float = 5.23 # float
+a3 = 'Hello friends' #str String
+a4 = True # bool Boolean
+
