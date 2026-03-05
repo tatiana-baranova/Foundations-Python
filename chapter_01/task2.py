829b7b1ea3215e5916896a17e2ea2bccf570438e
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-05 14:42:54 +0200

    variables

--- chapter_01/task2.py
+++ chapter_01/task2.py
@@ -1,12 +1,43 @@
-info = 50
-print("Result: ", info, "!", sep='')
-# del info
-info = 7
-print(info)
+# info = 50
+# print("Result: ", info, "!", sep='')
+# # del info
+# info = 7
+# print(info)
 
 # Типи данних
 a1: int = 500 #int Integer
 a2: float = 5.23 # float
-a3 = 'Hello friends' #str String
+a3 = '5' #str String
 a4 = True # bool Boolean
 
+print( a1 + int(a3))
+
+# print(float(a1) + float(a3))
+
+# a5 = 0
+# print(bool(a5))
+
+# x, y = 5, True
+# a = b = 10.5
+
+# a = "some"
+# print(a)
+
+# num1 = 5
+# num2 = 10.5
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num2 % num1)
+# num1 += 10
+
+# print(num1)
+
+# Отриманя даних
+
+num1 = int(input("Enter num 1: "))
+num2 = input("Enter num 2: ")
+print(num1 + int(num2))
+print(num1 ** int(num2))
+print(num1 // int(num2))
