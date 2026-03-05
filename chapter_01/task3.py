cd41feb923635e28eb169e2acad0468974864b9a
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-05 15:23:30 +0200

    conditional constructions

---
+++ chapter_01/task3.py
@@ -0,0 +1,16 @@
+# Умовні конструкції
+
+user = int(input("Enter some: "))
+hasBobCar = True
+
+if user >= 5 or not hasBobCar:
+    print("Hello")
+    num2 = int(input("Enter number: "))
+    if num2 <= 10:
+        print("Number < 10")
+elif user == 8:
+    print("Number is 8!")
+elif (user < 3 and user > -3) or not hasBobCar:
+    print("Between 3 and -3")
+else:
+    print("Incorrect number")
