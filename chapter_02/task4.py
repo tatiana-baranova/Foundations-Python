d9b80ca405ad5830c22fd0cc1573fdb16f43ee3a
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-06 13:39:14 +0200

    continue and break operators

--- chapter_02/task4.py
+++ chapter_02/task4.py
@@ -12,11 +12,15 @@
 #         isRunning = False
 # print("Finish!")
 
-for i in range(6, 11):
+# for i in range(6, 11):
+#     print(f"Element: {i}")
+
+
+# word = "Hello"
+# for el in word:
+#     if el == 'l': print('Found')
+
+for i in range(10, 20):
+    if i % 2 == 0: continue
     print(f"Element: {i}")
-
-
-word = "Hello"
-for el in word:
-    if el == 'l':
-        print('Found')
+    if i == 15: break
