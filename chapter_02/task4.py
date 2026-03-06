80e810654a639199fe8a103338441720c7ad7989
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-06 14:03:08 +0200

    working with text in loops

--- chapter_02/task4.py
+++ chapter_02/task4.py
@@ -52,3 +52,14 @@
 #     if i > maxElement: 
 #         maxElement = i
 # print(f"Max element: {maxElement}")
+
+# Робота з текстом
+
+# hobbies = 'skate,football,painting'
+# listHobbies = hobbies.split(',')
+# newListHobbies = []
+# for i in listHobbies:
+#     newListHobbies.append(i.capitalize())
+
+# res = ", ".join(newListHobbies)
+# print(res)
