baffa1c9e437161681749025e5e78556a6998720
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-06 13:33:39 +0200

    main types of cycles

---
+++ chapter_02/task4.py
@@ -0,0 +1,22 @@
+# Цикли в мові Python
+
+# i = 1000
+# while i > 10:
+#     print(f"Element: {i}")
+#     i /= 5
+
+# isRunning = True
+# while isRunning: 
+#     user = int(input('Enter the number 0: '))
+#     if user == 0:
+#         isRunning = False
+# print("Finish!")
+
+for i in range(6, 11):
+    print(f"Element: {i}")
+
+
+word = "Hello"
+for el in word:
+    if el == 'l':
+        print('Found')
