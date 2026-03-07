f260819bcf414577509385aa12a80fb4bd93f36e
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-07 18:41:37 +0200

    working with decorators

--- chapter_03/task2.py
+++ chapter_03/task2.py
@@ -1,3 +1,5 @@
+import time
+
 # Декоратор функцій
 # def auth(func):
 #     def wrapper():
@@ -33,19 +35,53 @@
 
 # # show()
 # profile()
-def multiply(x, y):
-    return x * y
 
 
-def double_result(func):
-    def wrapper(x, y):
-        result = func(x, y)
-        return result * 2
-    return wrapper
+# def multiply(x, y):
+#     return x * y
 
-@double_result
-def multiply(x, y):
-    return x * y
 
-result = multiply(6, 7)
-print(f"Результат множення з подвоєням: {result}")
+# def double_result(func):
+#     def wrapper(x, y):
+#         result = func(x, y)
+#         return result * 2
+#     return wrapper
+
+# @double_result
+# def multiply(x, y):
+#     return x * y
+
+# result = multiply(6, 7)
+# print(f"Результат множення з подвоєням: {result}")
+
+
+# def log_function(func):
+#     def wrapper(*args, **kwargs):
+#         print("Function started")
+#         result = func(*args, **kwargs)
+#         print("Function finished")
+#         return result
+#     return wrapper
+
+# @log_function
+# def say_hello():
+#     print("Hello")
+
+# say_hello()
+
+# def timer(func):
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         result = func(*args, **kwargs)
+#         end = time.time()
+#         execution_time = end - start
+#         print(f"Функція виконувалась: {execution_time:.2f} секунд")
+#         return result
+#     return wrapper
+
+# @timer
+# def slow_function():
+#     time.sleep(2)
+#     print("Функція виконана")
+
+# slow_function()
