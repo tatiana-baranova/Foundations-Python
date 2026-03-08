6dfa735cfdf276af12c41d742751c767d32bd82b
tetiana_baranova <syniovych.tanya@gmail.com>
2026-03-08 19:31:21 +0200

    work with error

--- chapter_03/task6.py
+++ chapter_03/task6.py
@@ -2,9 +2,12 @@
 
 try:
     num = int(input('Enter num: '))#file1.txt
-    f = open(f'data/file{num}.txt', 'r')
-    print(f.read())
-    f.close()
+    # f = open(f'data/file{num}.txt', 'r')
+    # print(f.read())
+    # f.close()
+
+    with open(f'data/file{num}.txt', 'r', encoding='utf-8') as f:
+        print(f.read())
 except FileNotFoundError:
     print("File not found")
 except ValueError:
